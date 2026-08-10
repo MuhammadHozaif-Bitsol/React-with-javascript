d286acaceb0d0bfe55866db2be54441a476a3722
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-10 12:47:02 +0500

    set up a reactfacts project

---
+++ ReactFacts/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import MainContent from "../components/MainContent";
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <MainContent></MainContent>
+    </div>
+  );
+}
+
+export default App;
