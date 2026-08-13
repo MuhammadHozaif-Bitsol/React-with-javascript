edb8489818c121f33216f200d52fc2d5571b7536
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-13 18:43:46 +0500

    started work on the meme generator app

---
+++ MemeGenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Main></Main>
+    </div>
+  );
+}
