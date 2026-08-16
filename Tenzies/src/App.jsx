5e3f4d9f32bff4b9877c7cbe52241115a8eeb09d
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-16 16:17:42 +0500

    added a roll button that generates new dice

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,19 +1,27 @@
 import Die from "./components/Die";
 import "./App.css";
+import { useState } from "react";
 export default function App() {
+  const [dieValues, setDieValues] = useState(generateAllNewDice);
+  function generateAllNewDice() {
+    let dieNumbers = [];
+    for (let i = 0; i < 10; i++) {
+      dieNumbers.push(Math.floor(Math.random() * 6 + 1));
+    }
+    return dieNumbers;
+  }
+  function rollDice() {
+    setDieValues(generateAllNewDice);
+  }
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {dieValues.map((val, index) => (
+          <Die key={index} value={val} />
+        ))}
+        <button className="roll-dice" onClick={rollDice()}>
+          Roll
+        </button>
       </div>
     </main>
   );
