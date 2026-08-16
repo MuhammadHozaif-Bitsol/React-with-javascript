b316d6cb438125a15328a3047af13951a9ff0251
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-16 15:15:48 +0500

    used the useEffect hook to fetch memes from the api and added the abiltiy to add bottom and top text as well to memes

--- MemeGenerator/src/components/Main.jsx
+++ MemeGenerator/src/components/Main.jsx
@@ -1,17 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   const [meme, setMeme] = useState({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
-    imageUrl: "http://i.imgflip.com/1bij.jpg",
+    imageUrl: "https://i.imgflip.com/1bij.jpg",
   });
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    async function getMemes() {
+      try {
+        const res = await fetch("https://api.imgflip.com/get_memes");
+        const data = await res.json();
+        setAllMemes(data.data.memes);
+      } catch (error) {
+        console.error("Failed to fetch memes:", error);
+      }
+    }
+    getMemes();
+  }, []);
+
+  function getRandomMeme() {
+    if (allMemes.length === 0) return;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const newMemeUrl = allMemes[randomNumber].url;
+    setMeme({
+      topText: "",
+      bottomText: "",
+      imageUrl: newMemeUrl,
+    });
+  }
 
   function handleChange(event) {
-    const { value } = event.currentTarget;
+    const { name, value } = event.currentTarget;
     setMeme((prevMeme) => ({
       ...prevMeme,
-      topText: value,
+      [name]: value,
     }));
   }
 
@@ -24,18 +49,25 @@ export default function Main() {
             type="text"
             placeholder="One does not simply"
             name="topText"
+            value={meme.topText}
             onChange={handleChange}
           />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getRandomMeme}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src={meme.imageUrl} />
+        <img src={meme.imageUrl} alt="Meme" />
         <span className="top">{meme.topText}</span>
         <span className="bottom">{meme.bottomText}</span>
       </div>
